dccc90d98bae26c63d1c6f2cf5b2b577d1a94687
Free10010 <297896911@qq.com>
2020-05-28 11:49:40 +0800

    submit spider notebook info

--- src/main/java/com/spiderZhuanZhuan/service/spiderService.java
+++ src/main/java/com/spiderZhuanZhuan/service/spiderService.java
@@ -17,19 +17,17 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Service
 @EnableScheduling
 public class spiderService {
     private static final Logger logger = LoggerFactory.getLogger(spiderService.class);
+    public static final String[] key = {"2650","E5-2670V2","E5-2689","E5-2660","i7-3770","i7-2600","i7-870","i5-3470","X5670","i7-2720QM","i7-950","X5660","i5-750","X5650","i5-760","X2-250", "i3-530", "E5620", "L5640", "i5-650", "X5550", "i3-2120", "E5645", "i3-3240", "i7-920XM","E5-2620","X5570","i7-860"};
     @Autowired
     RedisUtil redisUtil;
 
-    @Scheduled(fixedRate = 100000)
+    @Scheduled(fixedRate = 300000)
     public void crawl58ContentProduction() {
         List<ZhuanZhuanPojo> salePriceList = new ArrayList<>();
         List<ZhuanZhuanPojo> originPriceList = new ArrayList<>();
@@ -37,20 +35,7 @@ public class spiderService {
             try {
                 List<ZhuanZhuanPojo> zhuanPojoList = null;
                 CloseableHttpClient httpClient = HttpClients.createDefault(); // 创建HttpClient实例
-                HttpGet httpGet = new HttpGet("https://youpinapi.zhuanzhuan.com/goods/mall/complex/list/data?page=" + page + "&filtrate=5461:101%3B5462:2101002%3B5464:2%3B%3B&listType=29&from=zdqqa&init_from=&rstmark=1590551880444");//创建HttpGet实例
-//            httpGet.setHeader(":authority", "youpinapi.zhuanzhuan.com");
-//            httpGet.setHeader(":method", "GET");
-//            httpGet.setHeader(":path", "/goods/mall/complex/list/data?page=" + page + "&filtrate=5461:101%3B5462:2101002%3B5464:2%3B%3B5477:852089291349600377&listType=29&from=zdqqa&init_from=&rstmark=1590551880444");
-//            httpGet.setHeader(":scheme", "https");
-//            httpGet.setHeader("accept", "application/json, text/plain, */*");
-//            httpGet.setHeader("accept-encoding", "gzip, deflate, br");
-//            httpGet.setHeader("accept-language", "zh-CN,zh;q=0.9");
-//            httpGet.setHeader("cookie", "idzz=c5/nR17N2/td02mWGOx8Ag==; tk=c5/nR17N2/td02mWGOx8Ag==; id58=c5/nR17N3G1d02mWGTGaAg==; t=37; zz_t=37");
-//            httpGet.setHeader("origin", "https://m.zhuanzhuan.com");
-//            httpGet.setHeader("referer", "https://m.zhuanzhuan.com/u/activity/new-universal-activity?webview=zzn&key=zdqq&from=zdqqa&zzpage=ZZSHARESUCESE&zzfrom=LinkCopy&wxTime=1590551880260");
-//            httpGet.setHeader("sec-fetch-dest", "empty");
-//            httpGet.setHeader("sec-fetch-mode", "cors");
-//            httpGet.setHeader("sec-fetch-site", "same-site");
+                HttpGet httpGet = new HttpGet("https://youpinapi.zhuanzhuan.com/goods/mall/digital/list/data?page=" + page + "&filtrate=5461:103%3B5462:2103005%3B5464:2%3B5477:201912171734001&filterId=5464:2&rstmark=1590632599337&from=dnrc19&listType=4");//创建HttpGet实例
                 httpGet.setHeader("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1");
                 CloseableHttpResponse response;
                 response = httpClient.execute(httpGet);//执行HttpGet请求
@@ -84,21 +69,12 @@ public class spiderService {
                 zhuanPojoList = jsonArray.toJavaList(ZhuanZhuanPojo.class);
 
                 for (int i = 0; i < zhuanPojoList.size(); i++) {
-                    if (zhuanPojoList.get(i).getTitle().contains("荣耀20 ") ||zhuanPojoList.get(i).getTitle().contains("荣耀 30 ") ||zhuanPojoList.get(i).getTitle().contains("V20 ") ||zhuanPojoList.get(i).getTitle().contains("V30 ")|| zhuanPojoList.get(i).getTitle().contains("nova 5 Pro") || zhuanPojoList.get(i).getTitle().contains("nova 6 8") || zhuanPojoList.get(i).getTitle().contains("Mate 20 Pro") || zhuanPojoList.get(i).getTitle().contains("Mate 20 X") || zhuanPojoList.get(i).getTitle().contains("P30") || zhuanPojoList.get(i).getTitle().contains("nova 7")) {
-                        if (zhuanPojoList.get(i).getSalePrice()<2000&&!zhuanPojoList.get(i).getTitle().contains("8新以下")) {
-//                        if (!zhuanPojoList.get(i).getTitle().contains("8新以下")) {
-//                        if (zhuanPojoList.get(i).getSalePrice()<2000) {
-                            if (zhuanPojoList.get(i).getOriginPrice() != 0) {
-                                salePriceList.add(zhuanPojoList.get(i));
-                            }
-                            else {
-                                originPriceList.add(zhuanPojoList.get(i));
-                            }
+                    if (!Arrays.stream(key).anyMatch(zhuanPojoList.get(i).getTitle()::contains)) {
+                        salePriceList.add(zhuanPojoList.get(i));
 
-                        }
                     }
                 }
-                Thread.sleep(200);
+                Thread.sleep(100);
 //            logger.info("开始整理数据");
             } catch (Exception e) {
                 logger.error("error:{}", e);
